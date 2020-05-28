d6d6f96ca017e36914a5a8c88ad5f75b52f72524
Akash  Seth <seth@Akashs-MacBook-Pro.local>
2020-05-28 21:22:20 +0530

    Updates

---
+++ precache-manifest.5db2e0e2965e354c3da6b30880890b75.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35b8a309a978cb25685a02b78498b74b",
+    "url": "/React-Projects/index.html"
+  },
+  {
+    "revision": "c016c2724503137269b8",
+    "url": "/React-Projects/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "e5af84d690f3f3efc9bc",
+    "url": "/React-Projects/static/css/main.8029b970.chunk.css"
+  },
+  {
+    "revision": "c016c2724503137269b8",
+    "url": "/React-Projects/static/js/2.88b08eb1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Projects/static/js/2.88b08eb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5af84d690f3f3efc9bc",
+    "url": "/React-Projects/static/js/main.8a715725.chunk.js"
+  },
+  {
+    "revision": "b429c40102b921624459",
+    "url": "/React-Projects/static/js/runtime-main.c117848d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/React-Projects/static/media/SEGA.12a3db8b.woff"
+  }
+]);
